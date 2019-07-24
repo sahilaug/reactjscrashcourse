61be28436981af99f56e9b61902f83d481cf6a16
Sahil Sharma <sahilaug@gmail.com>
2019-07-25 04:32:40 +0530

    step19 wire context consumer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,60 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { ThemeContext } from "../App";
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <div
-          style={{
-            backgroundColor: this.props.curTheme === "dark" ? "black" : "white",
-            color: this.props.curTheme === "dark" ? "white" : "black",
-            height: "60px",
-            padding: "15px",
-            display: "flex",
-            justifyContent: "space-between"
-          }}
-        >
-          <div>
-            <NavLink
-              to="/"
-              exact
-              className="App-link"
-              activeClassName="active-link"
+        <ThemeContext.Consumer>
+          {value => (
+            <div
+              style={{
+                backgroundColor: value.theme === "dark" ? "black" : "white",
+                color: value.theme === "dark" ? "white" : "black",
+                height: "60px",
+                padding: "15px",
+                display: "flex",
+                justifyContent: "space-between"
+              }}
             >
-              Home
-            </NavLink>
-            <NavLink
-              to="/favourites"
-              className="App-link"
-              activeClassName="active-link"
-            >
-              Favourites
-            </NavLink>
-          </div>
+              <div>
+                <NavLink
+                  to="/"
+                  exact
+                  className="App-link"
+                  activeClassName="active-link"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/favourites"
+                  className="App-link"
+                  activeClassName="active-link"
+                >
+                  Favourites
+                </NavLink>
+              </div>
 
-          <div
-            style={{
-              fontSize: "20px",
-              fontWeight: "bold"
-            }}
-          >
-            {this.props.headerText}
-          </div>
-          <div>
-            <button
-              onClick={this.props.toggleTheme}
-              style={{ textTransform: "capitalize" }}
-            >
-              {this.props.curTheme}
-            </button>
-          </div>
-        </div>
+              <div
+                style={{
+                  fontSize: "20px",
+                  fontWeight: "bold"
+                }}
+              >
+                {this.props.headerText}
+              </div>
+              <div>
+                <button
+                  onClick={this.props.toggleTheme}
+                  style={{ textTransform: "capitalize" }}
+                >
+                  {this.props.curTheme}
+                </button>
+              </div>
+            </div>
+          )}
+        </ThemeContext.Consumer>
       </div>
     );
   }
