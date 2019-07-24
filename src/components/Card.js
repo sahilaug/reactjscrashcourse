5414a64da042f890c33f0ab04e9a924d6c913702
Sahil Sharma <sahilaug@gmail.com>
2019-07-25 03:32:09 +0530

    step14 wire card component styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Card = ({ cardData }) => {
   return (
@@ -8,10 +10,13 @@ const Card = ({ cardData }) => {
         width: "40%",
         border: "1px solid black",
         margin: "20px",
-        textAlign: "center"
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between"
       }}
     >
       <img
+        style={{ height: "200px", width: "200px", alignSelf: "center" }}
         alt={cardData.show.image ? cardData.show.image.medium : "thumbnail"}
         src={
           cardData.show.image
@@ -19,7 +24,18 @@ const Card = ({ cardData }) => {
             : "https://www.blackbeltkaratestudio.com/wp-content/uploads/2017/04/default-image-200x200.jpg"
         }
       />
-      <h1>{cardData.show.name}</h1>
+      <div
+        style={{
+          padding: "10px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <h1>{cardData.show.name}</h1>
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
     </div>
   );
 };
