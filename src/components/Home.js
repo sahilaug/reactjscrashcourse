f5943d83e425e4e1bb4edbb3df13014f29023d0b
Sahil Sharma <sahilaug@gmail.com>
2019-11-21 13:34:32 +0530

    remove useless hook use

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,9 @@
 import React from "react";
 import Card from "./Card";
-import useTheme from '../hooks/useTheme';
 
 const Home = ({ data, toggleLike }) => {
-  const [theme, toggleTheme] = useTheme();
   return (
     <div className="p20 card-container">
-      <button
-        onClick={toggleTheme}
-        style={{ textTransform: "capitalize", backgroundColor: theme === 'light' ? '#fff' : '#rgb(5.6%, 13.6%, 5.6%)' }}
-      >
-        {theme}
-      </button>
       {data.length
         ? data.map(d => (
           <Card
